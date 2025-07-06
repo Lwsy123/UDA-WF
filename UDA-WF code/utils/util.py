b103d62d8d91e802d8c8568b47d15a33f62dad2d
L123 <56485327+Lwsy123@users.noreply.github.com>
2025-07-06 22:14:55 +0800

    Update util.py

--- UDA-WF code/utils/util.py
+++ UDA-WF code/utils/util.py
@@ -2,26 +2,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-
-def transpose_qkv(data, head): #多头转换
-
-    b,c, h = data.shape
-
-    data = data.reshape(b,c, head, h//head)
-
-    data = data.transpose(1,2)
-
-    return data.reshape(-1, c, data.shape[-1])
-
-def transpose_output(data, head):
-
-    b,c, h = data.shape 
-    data = data.reshape(b // head, head, c, -1)
-
-    data = data.transpose(1,2)
-    return data.reshape(b // head, c, -1)
-
-
 def accurate(y_pred, y):
 
     indexes = torch.argmax(y_pred, dim=-1)
